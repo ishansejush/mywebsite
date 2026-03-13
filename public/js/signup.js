d924e3ac1f212635e336b8e9b2ce8cdf25d47446
ishansejush <ishansejush@gmail.com>
2026-03-13 21:13:42 +0530

    Update signup.js

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,1 +1,28 @@
 
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+const supabase = createClient(
+"https://uxkunzwpamfmkbddmavu.supabase.co",
+"sb_publishable_hSRnuCeMQ4I4IlPVjuw9Aw_kkWa7IAf"
+)
+
+window.signup = async function(){
+
+const email = document.getElementById("email").value
+const password = document.getElementById("password").value
+
+const { data, error } = await supabase.auth.signUp({
+email: email,
+password: password
+})
+
+if(error){
+console.error(error)
+alert("Signup failed: " + error.message)
+return
+}
+
+alert("Signup successful!")
+window.location="/login.html"
+
+}
